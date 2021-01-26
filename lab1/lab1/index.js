7e8611b4f23b158ac289ec5584754183e81666e6
Jonathan J Montalvo Colon <jmontalvo@knights.ucf.edu>
2021-01-25 22:33:59 -0500

    lab1

---
+++ lab1/lab1/index.js
@@ -0,0 +1,13 @@
+const Jonathan = require("./Jonathan.js");
+
+let p = new Jonathan ("Jonathan", [
+    "#7D0000",
+    "#FF6800",
+    "#FFAD00"
+], [
+    "#00F9FF",
+    "#83C9FF",
+    "#0057FF"
+]);
+
+p.speak();
