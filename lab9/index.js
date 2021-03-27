aac6ad7dff11e5fd17640d4c64a881d1e1e8e531
Jonathan J Montalvo <36077725+xAVENG3Rx@users.noreply.github.com>
2021-03-26 22:09:39 -0400

    lab9

---
+++ lab9/index.js
@@ -0,0 +1,54 @@
+import Express from "express";
+import fs from "fs";
+
+const App = Express();
+const port = 3010;
+
+let fileContents = fs.readFileSync("database.json");
+
+let database = JSON.parse(fileContents);
+
+App.use("/", Express.static("public"));
+
+App.get("/employees/name/:name", (req,res) => {
+    let result = {"error": "not found"};
+
+    database.forEach((value) => {
+        if(req.params.name == value.name) {
+            result = value;
+        }
+    });
+
+    res.json(result);
+})
+
+App.get("/employees/ages/:age", (req,res) => {
+    let result = {"error": "not found"};
+
+    database.forEach((value) => {
+        if(req.params.age == value.age) {
+            result = value;
+        }
+    });
+
+    res.json(result);
+})
+
+App.post("/employees/:name/:age", (req, res) => {
+    let result = {
+        "name": req.params.name,
+        "age": parseInt(req.params.age)
+    };
+
+    database.push(result);
+
+    fs.writeFileSync("database.json", JSON.stringify(database, null, '\t'));
+
+    res.json(result);
+});
+
+App.listen(port, () => {
+    console.log("Server running!");
+})
+
+
